d4fe7daf89c3b8513255ef9fcc0eabf74c589bf4
jennkini <jennkini@b2757b41-e7bf-d0c8-ca89-896bb4b1b68f>
2010-06-19 00:47:25 +0000

    added  admin home php file

---
+++ trunk/admin/admin_home/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<link rel="stylesheet" type="text/css" href="admin_header.css" /> 
+<link rel="stylesheet" type="text/css" href="admin_home.css" />
+<title>WTBest Admin Home</title>
+</head>
+
+<body>
+	<h1 class="admin_title">
+		WTBest Administration ~ Email List
+	</h1>
+	<div class="navigation">
+		<ul>
+        	<li><a href="../admin_home/">Email List</a></li>
+            <li><a href="../admin_events/">Add/Edit Events</a></li>
+            <li><a href="../admin_sponsors/">Add/Edit Sponsors</a></li>
+            <li><a href="../admin_documents/">Add/Edit Documents</a></li>
+        </ul>
+	</div>
+	<div class="content">
+		<h2>Email List</h2>
+		<table>
+			<tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Organization Name</th><th>Organization Type</th><th>Zip</th><th>Delete</th></tr>
+			<?php
+				i=array(email_list);
+				foreach ($i as $value)
+					{
+					 <tr>
+						<td>echo $value.first_name</td>
+						<td>echo $value.last_name</td>
+						<td>echo $value.email</td>
+						<td>echo $value.org_name</td>
+						<td>echo $value.org_type</td>
+						<td>echo $value.org_zip</td>
+						<td><input type="submit" value="Delete" /></td>
+					 </tr>
+					 }
+				?>
+			<form action="email_list.php" method="get">
+				<tr>
+					<td><input type="text" name="first_name" /></td>
+					<td><input type="text" name="last_name" /></td>
+					<td><input type="text" name="email" /></td>
+					<td><input type="text" name="org_name" /></td>
+					<td><input type="text" name="org_type" /></td>
+					<td><input type="text" name="org_zip" /></td>
+					<td><input type="submit" value="Add" /></td>
+				</tr>
+			</form>
+		</table>
+	</div>
+</body>
+
+</html>
