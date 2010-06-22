bdefc287a606823a9f56f47ae67569f6d27c584a
glenrhunt <glenrhunt@b2757b41-e7bf-d0c8-ca89-896bb4b1b68f>
2010-06-22 20:46:02 +0000

    clearing source tree

--- trunk/include/php/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-function genli($url, $text, $subtext) {
-    echo '<li><a href="' . $url . '">' . $text . '<small>' . $subtext . '</small></a></li>';
-} ?>
--- trunk/master/nav.php
+++
@@ -1,6 +0,0 @@
-<?php // note: genli($url, $title, $subtitle) is defined in include/php/functions.php ?>
-<?php echo genli('../home/', 'Home', '&nbsp;'); ?>
-<?php echo genli('../events/', 'Events', 'Upcoming Deadlines'); ?>
-<?php echo genli('../sponsors/', 'Sponsors', 'Support WTBEST'); ?>
-<?php echo genli('../documents/', 'Documents', 'Important Paperwork'); ?>
-<?php echo genli('../contact/', 'Contact Us', 'Questions and Comments'); ?>
