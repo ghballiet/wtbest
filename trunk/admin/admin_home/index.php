dacfd4027b143fe630f9780640a9fcb5fb179c58
jennkini <jennkini@b2757b41-e7bf-d0c8-ca89-896bb4b1b68f>
2010-06-22 17:50:16 +0000

    modified for only 1 css file for all

--- trunk/admin/admin_home/index.php
+++ trunk/admin/admin_home/index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
+<!-- ADMIN EMAIL LIST -->
+
 <head>
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<link rel="stylesheet" type="text/css" href="admin_header.css" /> 
-<link rel="stylesheet" type="text/css" href="admin_home.css" />
+<link rel="stylesheet" type="text/css" href="../include/admin_header.css" /> 
 <title>WTBest Admin Home</title>
 </head>
 
@@ -14,10 +15,10 @@
 	</h1>
 	<div class="navigation">
 		<ul>
-        	<li><a href="../admin_home/">Email List</a></li>
-            <li><a href="../admin_events/">Add/Edit Events</a></li>
-            <li><a href="../admin_sponsors/">Add/Edit Sponsors</a></li>
-            <li><a href="../admin_documents/">Add/Edit Documents</a></li>
+        	<li><a href="../admin_home/index.php">Email List</a></li>
+            <li><a href="../admin_events/index.php">Add/Edit Events</a></li>
+            <li><a href="../admin_sponsors/index.php">Add/Edit Sponsors</a></li>
+            <li><a href="../admin_documents/index.php">Add/Edit Documents</a></li>
         </ul>
 	</div>
 
@@ -52,8 +53,15 @@
 					<td><input type="text" name="last_name" /></td>
 					<td><input type="text" name="email" /></td>
 					<td><input type="text" name="org_name" /></td>
-					<td><input type="text" name="org_type" /></td>
-					<td><input type="text" name="org_zip" /></td>
+					<td>
+						<select id="org_type" name="org_type">
+							<option value="">--------------------</option>
+							<option value="jrhigh">Junior High School</option>
+							<option value="srhigh">High School</option>
+							<option value="sponsor">Sponsor</option>
+							<option value="other">Other</option>
+                        </select>
+                    </td>
 					<td><input type="submit" value="Add" /></td>
 				</tr>
 			</form>
