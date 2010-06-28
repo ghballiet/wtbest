6bd5deac5fe179ef38ac10bcd9988105adac7bb6
glenrhunt <glenrhunt@b2757b41-e7bf-d0c8-ca89-896bb4b1b68f>
2010-06-28 23:02:55 +0000

    FIXED LOGIN

--- admin/include/header.php
+++ admin/include/header.php
@@ -1,6 +1,7 @@
-<?php $SECRET_KEY="9RatrE9ded9he8"; ?>
-<?php if(!isset($_COOKIE[$SECRET_KEY])) {
-    header('Location: ../..'); 
+<?php
+$SECRET_KEY="9RatrE9ded9he8";
+if(!isset($_COOKIE[$SECRET_KEY])) {
+    header('Location: ../'); 
 }?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php require_once('database.php'); ?>
