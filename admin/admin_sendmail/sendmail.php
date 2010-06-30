55587d235317f56ddd477a13c03658577738584d
jennkini <jennkini@b2757b41-e7bf-d0c8-ca89-896bb4b1b68f>
2010-06-30 23:43:11 +0000

    update sendmail.php

--- admin/admin_sendmail/sendmail.php
+++ admin/admin_sendmail/sendmail.php
@@ -8,8 +8,13 @@ $link=db_connect();
 		$org_type = $_REQUEST['org_type'];
 		$subject = $_REQUEST['subject'];
 		$message = $_REQUEST['message'];
-		$mail_list = mysql_query("SELECT email FROM mailing_list WHERE org_type=$org_type");
-		mail($mail_list,$subject,$message,"From: donotreply@wtbest.org");
+		$person_id = $_REQUEST['name'];
+		$list1 = "SELECT email FROM mailing_list WHERE org_type=$org_type";
+		$mail_list1 = db_query($list1);
+		mail($mail_list1,$subject,$message,"From: donotreply@wtbest.org");
+		$list2 = "SELECT email FROM mailing_list WHERE id=$person_id";
+		$mail_list2 = db_query($list1);
+		mail($mail_list2,$subject,$message,"From: donotreply@wtbest.ort");
 	)
 	else //email not filled in
 	{
