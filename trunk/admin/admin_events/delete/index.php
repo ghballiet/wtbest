80440606e976c42f554e139fb1372ce62594b3e3
jennkini <jennkini@b2757b41-e7bf-d0c8-ca89-896bb4b1b68f>
2010-07-06 04:01:09 +0000

    update admin event site for add edit delete functions

--- trunk/admin/admin_events/delete/index.php
+++ trunk/admin/admin_events/delete/index.php
@@ -1,11 +1,12 @@
 <?php
 require_once('../../include/database.php');
 $link=db_connect();
-$q = "SELECT * FROM events e WHERE e.id='" . $_POST['id'] . "';";
-$list = db_query($q);
+//$q = "SELECT * FROM events e WHERE id='" . $_POST['id'] . "';";
+//$list = db_query($q);
 db_update("DELETE FROM events WHERE id='" . $_POST['id'] . "';");
-$date = date_format(date_create($list[1][date]),"l F j Y");
-$time = date_format(date_create($list[1][time]),"h:iA");
-echo "Deleted event on " . $date . " at " . $time . ".";
+//$date = date_format(date_create($list[1][date]),"l F j Y");
+//$time = date_format(date_create($list[1][time]),"h:iA");
+//echo "Deleted event on " . $date . " at " . $time . ".";
 db_disconnect($link);
 ?>
+<?php header('Location: ../?msg=Update succeeded.'); ?>
