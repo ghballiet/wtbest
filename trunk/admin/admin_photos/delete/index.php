643ed5b9209000a1747d1ee7c48b2f276866d5d0
jennkini <jennkini@b2757b41-e7bf-d0c8-ca89-896bb4b1b68f>
2010-07-07 03:38:54 +0000

    photo admin working for all add edit delete

--- trunk/admin/admin_photos/delete/index.php
+++ trunk/admin/admin_photos/delete/index.php
@@ -1,6 +1,5 @@
 <?php require_once('../../include/database.php'); ?>
-<?php $link=db_connect();
-db_update("DELETE FROM photos where id='" . $_POST['id'] . "';");
-?>
-<?php db_disconnect($link);
-header("Location: .."); ?>
+<?php $link=db_connect(); ?>
+<?php db_update("DELETE FROM photos where id='" . $_POST['id'] . "';"); ?>
+<?php db_disconnect($link); ?>
+<?php header('Location: ../?msg=Update succeeded.'); ?>
--- trunk/admin/admin_photos/update/index.php
+++ trunk/admin/admin_photos/update/index.php
@@ -1,16 +1,14 @@
 <?php require_once('../../include/database.php'); ?>
 <?php $link=db_connect(); ?>
-	<?php
-		$update_str = " ";
-		if($_POST[photo] != "") { } // TODO: this is gonna be tricky, we should do this part later
-		if($_POST[caption] != "") { $update_str = $update_str . " caption='" . $_POST[caption] . "', "; }
-		$update_str = substr(trim($update_str),0,-1); // removing extra ", "
-		//db_udpdate("UPDATE photos SET photo='" . $_POST[photo] . "', caption='" . $_POST[caption] . "'
-		//			WHERE id='" . $_POST['id'] . "';");
-		if($update_str != "") {
-			db_update("UPDATE photos SET " . $update_str . " where id='" . $_POST[id] . "';");
-		}
-	?>
-	
+<?php
+$update_str = " ";
+if($_POST[photo] != "") { } // TODO: this is gonna be tricky, we should do this part later
+if($_POST[caption] != "") { $update_str = $update_str . " caption='" . $_POST[caption] . "', "; }
+$update_str = substr(trim($update_str),0,-1); // removing extra ", "
+
+if($update_str != "") {
+	db_update("UPDATE photos SET " . $update_str . " where id='" . $_POST['id'] . "';");
+}
+?>
 <?php db_disconnect($link); ?>
 <?php header('Location: ../?msg=Update succeeded.'); ?>
