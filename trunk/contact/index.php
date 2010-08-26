bb4ee86bf93e7aa32f42c791590ac60de0469be0
jennkini <jennkini@b2757b41-e7bf-d0c8-ca89-896bb4b1b68f>
2010-08-26 21:23:01 +0000

    remove contact us form and include an email

--- trunk/contact/index.php
+++ trunk/contact/index.php
@@ -5,19 +5,9 @@
 <?php require('../master/header.php'); ?>
 
                     <!-- content starts here -->
-                    <form id="contact" method="post" action="contact_send.php">
-                        <table>
-                            <tr><th>First Name</th><th>Last Name</th></tr>
-                            <tr><td><input type="text" name="first_name" id="first_name" /></td><td><input type="text" name="last_name" id="last_name" /></td></tr>
-                        </table>
-                        <table>
-                            <tr><th>Email Address</th></tr>
-                            <tr><td><input type="text" name="email" id="email" /></td></tr>
-                            <tr><th>Message</th></tr>
-                            <tr><td><textarea name="message" id="message" /></textarea></td></tr>
-                            <tr><td><input type="submit" value="Send" /></td></tr>
-                        </table>
-                    </form>
+                    
+					Contact WTBest through michael.helm@ttu.edu with any questions or comments.
+					
                     <!-- end of content -->
 
 <?php require('../master/footer.php'); ?>
